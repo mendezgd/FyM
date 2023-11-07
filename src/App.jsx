8c220ed167dd1d1dcd989e21fd9f17ec88fe6f39
gmendez <mendezgd88@gmail.com>
2023-11-07 19:23:40 -0300

    css form y map

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 /* import AnimateOnScroll from './components/AnimateOnScroll'; */
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Formulario from './components/Formulario';
+import GoogleMap from './components/GoogleMap';
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Formulario />
+      <GoogleMap />
       {/* <AnimateOnScroll /> */}
     </>
   )
