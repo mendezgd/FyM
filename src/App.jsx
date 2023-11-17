fa0339fd9474d43f238dd0a6c5fa8559e4d75f4d
gmendez <mendezgd88@gmail.com>
2023-11-17 13:51:05 -0300

    testing media

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
-/* import AnimateOnScroll from './components/AnimateOnScroll'; */
+import AnimateOnScroll from './components/AnimateOnScroll';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Formulario from './components/Formulario';
-import GoogleMap from './components/GoogleMap';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Navbar />
       <Header />
       <Formulario />
-      <GoogleMap />
       {/* <AnimateOnScroll /> */}
     </>
   )
