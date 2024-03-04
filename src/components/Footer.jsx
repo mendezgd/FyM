7aac3837c1f97b28488bbdd24c9641fe607426d0
gmendez <mendezgd88@gmail.com>
2024-03-04 15:38:31 -0300

    links footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,17 +38,17 @@ const Footer = () => {
                     <div className="col-8 col-lg-2 mb-3">
                         <h5 className="footerTitle">Info</h5>
                         <ul className="list-unstyled">
-                            <li className="navItemFooter">11-2118-7415<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style={{ fill: 'black', transform: "", msFilter: "" }}>
+                            <li><a className="navItemFooter" href="tel:+541121187415">11-2118-7415</a><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style={{ fill: 'black', transform: "", msFilter: "" }}>
                                 <path fillRule="evenodd" clipRule="evenodd" d="M17.707 12.293a.999.999 0 0 0-1.414 0l-1.594 1.594c-.739-.22-2.118-.72-2.992-1.594s-1.374-2.253-1.594-2.992l1.594-1.594a.999.999 0 0 0 0-1.414l-4-4a.999.999 0 0 0-1.414 0L3.581 5.005c-.38.38-.594.902-.586 1.435.023 1.424.4 6.37 4.298 10.268s8.844 4.274 10.269 4.298h.028c.528 0 1.027-.208 1.405-.586l2.712-2.712a.999.999 0 0 0 0-1.414l-4-4.001zm-.127 6.712c-1.248-.021-5.518-.356-8.873-3.712-3.366-3.366-3.692-7.651-3.712-8.874L7 4.414 9.586 7 8.293 8.293a1 1 0 0 0-.272.912c.024.115.611 2.842 2.271 4.502s4.387 2.247 4.502 2.271a.991.991 0 0 0 .912-.271L17 14.414 19.586 17l-2.006 2.005z"></path>
                             </svg></li>
-                            <li className="navItemFooter">ventas@fymsanitarios.com<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style={{ fill: 'black', transform: "", msFilter: "" }}>
+                            <li><a className="navItemFooter" href="mailto:ventas@fymsanitarios.com">ventas@fymsanitarios.com</a><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style={{ fill: 'black', transform: "", msFilter: "" }}>
                                 <path d="M20 4H4c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zm0 2v.511l-8 6.223-8-6.222V6h16zM4 18V9.044l7.386 5.745a.994.994 0 0 0 1.228 0L20 9.044 20.002 18H4z"></path>
                             </svg></li>
                         </ul>
                     </div>
                     <div className="col-4 col-lg-2 mb-3">
                         <a href="http://qr.afip.gob.ar/?qr=J1bWr3ey9w04iYRnN4HQMA,," target="_F960AFIPInfo">
-                            <img src="https://www.afip.gob.ar/images/f960/DATAWEB.jpg" width="50" height="70" border="0" alt="Afip"/>
+                            <img src="https://www.afip.gob.ar/images/f960/DATAWEB.jpg" width="50" height="70" border="0" alt="Afip" />
                         </a>
                     </div>
                 </div>
